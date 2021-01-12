5cd5e7b779ff52055f7332998ad2a5be9a2e9f21
Leroy <leroy.mukiibi@gmail.com>
2021-01-12 11:25:50 +0100

    Add project files.

---
+++ Inlämningsuppgift_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inlämningsuppgift_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console_Monsters CM = new Console_Monsters();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Inlämningsuppgift_3/Texts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inlämningsuppgift_3
+{
+    class Texts
+    {
+        public const string Welcome =
+            "********************************\n" +
+            "* Welcome to Console Monsters! *\n" +
+            "********************************\n";
+    }
+}
