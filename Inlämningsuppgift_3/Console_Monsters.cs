78f4c32c47c53f91c9880a8aef37c6b97c60e6e8
Leroy <leroy.mukiibi@gmail.com>
2021-01-12 13:41:12 +0100

    Getting the battle logics in place

--- Inlämningsuppgift_3/Console_Monsters.cs
+++ Inlämningsuppgift_3/Console_Monsters.cs
@@ -2,15 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Inlämningsuppgift_3
 {
     class Console_Monsters
     {
+        private bool running;
         public Console_Monsters()
         {
-            Console.WriteLine(Texts.Welcome);
+            running = true;
+            Message(Texts.Welcome);
+            Thread.Sleep(500);
+            Run();
+        }
+
+        private void Exploring()
+        {
+            Console.Clear();
+            Random random = new Random();
+            if (random.Next(0, 10) > 8)
+            {
+
+            }
+        }
+
+        private void Run()
+        {
+            while (running)
+            {
+                int choise;
+                Message(Texts.StartMenu);
+                try
+                {
+                    choise = Convert.ToInt32(Console.ReadLine());
+
+                    switch (choise)
+                    {
+                        case 1:
+                            Exploring();
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+                            break;
+                        case 4:
+                            running = false;
+                            Message(Texts.Exit);
+                            break;
+                        default:
+                            Message(Texts.WrongInput);
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Message(Texts.WrongInput);
+                }
+            }
+        }
+
+        private void Message(string m)
+        {
+            Console.WriteLine(m);
         }
     }
 }
--- Inlämningsuppgift_3/Texts.cs
+++ Inlämningsuppgift_3/Texts.cs
@@ -12,5 +12,26 @@ namespace Inlämningsuppgift_3
             "********************************\n" +
             "* Welcome to Console Monsters! *\n" +
             "********************************\n";
+
+        public const string StartMenu =
+            "1. Explore the world\n" +
+            "2. Character info\n" +
+            "3. Go to the shop\n" +
+            "4. Exit";
+
+        public const string WrongInput =
+            "Wrong input";
+
+        public const string Exit =
+            "You have been up too long. It's time to go to bed. See you in the morning.";
+
+        public const string FoundNothing =
+            "You've been walking around for a while and found nothing..";
+
+        public const string Encounter =
+            "Oh oh!! You encountered a ";
+
+        public const string BeginBattle =
+            "Begin battle!";
     }
 }
